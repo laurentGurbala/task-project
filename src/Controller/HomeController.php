73c786ddfcc66a03c7e5c067b966a159ff2bd141
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-10-03 14:21:05 +0200

    feat: ajout de la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,17 +3,26 @@
 namespace App\Controller;
 
 use App\Enum\NavbarVariant;
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
+    public function __construct(
+        private ProjectRepository $projectRepository,
+        private EntityManagerInterface $entityManager,
+    ) {}
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $projects = $this->projectRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            "controller_name" => 'HomeController',
+            'projects' => $projects,
             'navbar_variant' => NavbarVariant::HOME,
         ]);
     }
