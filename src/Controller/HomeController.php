da937fac5b2197656f3f22528bc10de11633592d
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2025-10-06 05:01:39 +0200

    feat: ajout d'un projet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Enum\NavbarVariant;
+use App\Form\ProjectType;
 use App\Repository\ProjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,6 +26,28 @@ final class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'projects' => $projects,
+            'navbar_variant' => NavbarVariant::HOME,
+        ]);
+    }
+
+    public function add(
+        Request $request,
+    ): Response
+    {
+        $project = new Project();
+
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($project);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/add.html.twig', [
+            'form' => $form->createView(),
             'navbar_variant' => NavbarVariant::HOME,
         ]);
     }
---
+++ src/Form/ProjectType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('createdAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text'
+            ])
+            ->add('owner', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('members', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
